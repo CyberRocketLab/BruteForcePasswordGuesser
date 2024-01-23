167db6f81ec20e6b197b17ad7b2a0c6c9ed33816
Alexandr Curanov <a12029720@univie.ac.at>
2024-01-23 11:19:14 +0100

    Fixing the Problem with memory space. No longer Storing the passwords

--- src/main/java/org/example/generator/NumberGuesser.java
+++ src/main/java/org/example/generator/NumberGuesser.java
@@ -6,28 +6,39 @@ import java.util.List;
 public class NumberGuesser {
 
     private final int digitSize;
+    private boolean stopGeneration = false;
 
     public NumberGuesser(int digitSize) {
         this.digitSize = digitSize;
     }
 
-    public List<String> getPasswordList() {
-        List<String> passwordList = new ArrayList<>();
+    public void generateAndUsePassword(PasswordConsumer consumer) {
         String emptyStartDigit = "";
-        generatePasswords(emptyStartDigit, digitSize, passwordList);
-
-        return passwordList;
+        generatePasswords(emptyStartDigit, digitSize, consumer);
     }
 
-    private void generatePasswords(String current, int size, List<String> list) {
+    private void generatePasswords(String current, int size, PasswordConsumer consumer) {
+        if (stopGeneration) {
+            return; // Stop generating further passwords
+        }
+
         if (size == 0) {
-            list.add(current);
+            consumer.consume(current);
             return;
         }
 
         for (int i = 0; i < 10; i++) {
-            generatePasswords(current + i, size - 1, list);
+            generatePasswords(current + i, size - 1, consumer);
         }
+    }
+
+    public void setStopGeneration(boolean stopGeneration) {
+        this.stopGeneration = stopGeneration;
+    }
+
+    @FunctionalInterface
+    public interface PasswordConsumer {
+        void consume(String password);
     }
 
 
--- src/main/java/org/example/unlocker/PDFUnlocker.java
+++ src/main/java/org/example/unlocker/PDFUnlocker.java
@@ -4,40 +4,35 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.example.generator.NumberGuesser;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.List;
 
 public class PDFUnlocker {
 
-    private NumberGuesser numberGuesser;
+    private final NumberGuesser numberGuesser;
 
     public PDFUnlocker(NumberGuesser numberGuesser) {
         this.numberGuesser = numberGuesser;
     }
 
-    public boolean unlockPDF(File file) {
+    public void unlockPDF(File file) {
         try {
             byte[] fileContent = Files.readAllBytes(file.toPath());
 
-            List<String> passwordList = numberGuesser.getPasswordList();
-
-            for (String eachPassword : passwordList) {
+            numberGuesser.generateAndUsePassword(eachPassword -> {
                 if (tryUnlockFile(fileContent, eachPassword)) {
                     System.out.println("\nPassword was found : " + eachPassword);
-                    break;
+                    numberGuesser.setStopGeneration(true);
                 } else {
                     System.out.println("Password - " + eachPassword + " is incorrect");
                 }
-            }
+            });
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return true;
     }
 
     private boolean tryUnlockFile(byte[] fileContent, String password) {
