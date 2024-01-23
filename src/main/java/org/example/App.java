28fbefef88bcf540578fea385e6d42dba60a5af4
Alexandr Curanov <a12029720@univie.ac.at>
2024-01-23 11:40:22 +0100

    Updating Main File so it will accept File Path from user

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,23 +4,34 @@ import org.example.generator.NumberGuesser;
 import org.example.unlocker.PDFUnlocker;
 
 import java.io.File;
+import java.util.Scanner;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        File file = new File("/Users/rocket/Desktop/ZIP/Homework 1.pdf");
+public class App {
+    private static final int MAX_DIGITS = 5;
 
-        NumberGuesser numberGuesser = new NumberGuesser(4);
-        PDFUnlocker pdfUnlocker = new PDFUnlocker(numberGuesser);
-        pdfUnlocker.unlockPDF(file);
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cracking PDF script!");
+        System.out.println("The Script will try to guess the password up to 5 digits");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the path to the PDF file:");
+        String filePath = scanner.nextLine();
+        File file = new File(filePath);
 
 
+        for (int digit = 1; digit <= MAX_DIGITS; ++digit) {
+            System.out.println("Trying password with " + digit + " digits!");
+            NumberGuesser numberGuesser = new NumberGuesser(digit);
+            PDFUnlocker pdfUnlocker = new PDFUnlocker(numberGuesser);
+
+            if (pdfUnlocker.unlockPDF(file)) {
+                System.out.println("Thank you!");
+                break;
+            }
+        }
 
     }
 }
